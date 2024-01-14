95c0534178fb98ed6d84bc820115b1c45be7f0a8
occoli <155591951+occoli@users.noreply.github.com>
2024-01-14 21:54:53 +0900

    Update 세로읽기.java

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -7,7 +7,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         char[][] arr = new char[5][15];
-
+        // 각 줄별로 주어지는 문자를 받고 그 문자의 길이만큼반 반복하여 한글자씩 저장
         for (int i = 0; i < 5; i++) {
             String str = br.readLine();
             for (int j = 0; j < str.length(); j++) {
@@ -17,6 +17,7 @@ public class Main {
 
         String str = "";
 
+        // 세로로 돌면서 글자를 하나씩 str에 붙여넣는다. 단, 비어있을 경우에는 건너뛰기 (char형 배열은 공백일경우 '\u1000'으로 표현)
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 5; j++) {
                 if(arr[j][i] == '\u0000'){
@@ -26,9 +27,6 @@ public class Main {
                 }
             }
         }
-
         System.out.println(str);
-
     }
-
 }
