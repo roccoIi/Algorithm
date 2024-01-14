c2e1426903f532543964c1868e5c5d3be7df4ad0
occoli <155591951+occoli@users.noreply.github.com>
2024-01-14 21:52:11 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -8,23 +8,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
+        // 기본이 되는 도화지 그리기 (100 x 100 사이즈)
         int[][] arr = new int[100][100];
         int time = Integer.parseInt(br.readLine());
         int sum = 0;
 
+        // 주어지는 색종이의 수만큼 반복
         for (int i = 0; i < time; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int width = Integer.parseInt(st.nextToken());
             int height = Integer.parseInt(st.nextToken());
-
+        // 주어지는 위치에 붙이는 색종이를 0 -> 1 로 표현한다. (중복되어도 1 -> 1 이기 때문에 영향을 받지 않는다.)
             for (int j = width; j < width+10; j++) {
                 for (int k = height; k < height + 10; k++) {
                     arr[j][k] = 1;
                 }
             }
         }
-
+        // 도화지(배열)에 칠해진 1의 개수(색종이)를 다 더한다.
         for (int j = 0; j < 100; j++) {
             for (int k = 0; k < 100; k++) {
                 sum += arr[j][k];
@@ -34,12 +35,5 @@ public class Main {
         br.close();
 
         System.out.println(sum);
-
-
-
-
-
     }
-
-
 }
