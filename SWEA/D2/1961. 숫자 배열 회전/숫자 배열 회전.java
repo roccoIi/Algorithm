9c46d332ccb983e7fada7eb346bff1915ba7b7d0
occoli <155591951+occoli@users.noreply.github.com>
2024-01-18 17:39:04 +0900

    [D2] Title: 숫자 배열 회전, Time: 147 ms, Memory: 20,856 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,37 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
-		
-		int T = sc.nextInt();
-		for(int i = 1; i <= T; i++) {
-			int a = sc.nextInt();
-			int[][] arr = new int[a][a];
-			
-			// 제시되는 배열 만들기
-			for(int r = 0; r<a;r++) {
-				for (int c = 0; c<a;c++) {
-					arr[r][c] = sc.nextInt();
-				}
-			}
-			
-			System.out.printf("#%d\n", i);
-			
-			for(int r = 0; r < a; r++) {
-				for(int c = 0; c < a; c++) {
-					System.out.print(arr[a-c-1][r]);
-				}
-				System.out.print(" ");
-				for(int c = 0; c<a; c++) {
-					System.out.print(arr[a-r-1][a-c-1]);
-				}
-				System.out.print(" ");
-				for(int c = 0; c < a; c++) {
-					System.out.print(arr[c][a-r-1]);
-				}
-				System.out.println();
-			}
-		}
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= T; i++) {
+            int a = Integer.parseInt(br.readLine());
+            int[][] arr = new int[a][a];
+            
+            // 제시되는 배열 만들기
+            for(int r = 0; r<a;r++) {
+            	StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int c = 0; c<a;c++) {
+                    arr[r][c] = Integer.parseInt(st.nextToken());
+                }
+            }
+            
+            System.out.printf("#%d\n", i);
+            
+            for(int r = 0; r < a; r++) {
+                for(int c = 0; c < a; c++) {
+                    System.out.print(arr[a-c-1][r]);
+                }
+                System.out.print(" ");
+                for(int c = 0; c<a; c++) {
+                    System.out.print(arr[a-r-1][a-c-1]);
+                }
+                System.out.print(" ");
+                for(int c = 0; c < a; c++) {
+                    System.out.print(arr[c][a-r-1]);
+                }
+                System.out.println();
+            }
+        }
 	}
 }
