74efac3afeda88ad035b8429c98cfe34c74194da
occoli <155591951+occoli@users.noreply.github.com>
2024-02-06 12:10:40 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 113 ms, Memory: 18,656 KB -BaekjoonHub

---
+++ SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int t = 1; t <= 10; t++) {
+			Stack<Character> stack = new Stack<>();
+			int length = Integer.parseInt(br.readLine());
+			String str = br.readLine();
+			String temp = "";
+			int sum = 0;
+			
+			
+			// 후위표기식 작성
+			for(int i = 0; i < length; i++) {
+				char c = str.charAt(i);
+				if('0'<=c && c<='9') {
+					temp += c;
+				} else {
+					if(stack.isEmpty()) {
+						stack.push(c);
+					} else {
+						while(!stack.isEmpty()) {
+							temp += stack.pop();
+						}
+						stack.push(c);
+					}
+				}
+			}
+			temp += stack.pop();
+			// 후위표기식 계산
+			Stack<Integer> newStack = new Stack<>();
+			for(int i = 0; i < length; i++) {
+				int c = temp.charAt(i)-'0';
+				
+				if(0 <= c && c <= 9) {
+					newStack.push(c);
+				} else {
+					sum = newStack.pop() + newStack.pop();
+					newStack.push(sum);
+				}
+			}
+			System.out.printf("#%d %d\n", t, sum);
+		}
+	}
+}
