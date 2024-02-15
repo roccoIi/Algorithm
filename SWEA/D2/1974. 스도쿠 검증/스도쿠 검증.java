5f39cc4f1add0b5502dbe9eecafa4c6069a3049d
occoli <155591951+occoli@users.noreply.github.com>
2024-02-15 16:27:33 +0900

    [D2] Title: 스도쿠 검증, Time: 117 ms, Memory: 18,964 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,78 +1,74 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
-        int[][] arr = new int[9][9];
-
-        //델타 배열
-        int[] dr = {-1, 1, 0, 0, -1, -1, 1, 1};
-        int[] dc = {0, 0, -1, 1, -1, 1, -1, 1};
-
-        for (int k = 1; k <= T; k++) {
-            boolean flag= false;
-            // 스도쿠 채우기
-            for (int r = 0; r < 9; r++) {
-                for (int c = 0; c < 9; c++) {
-                    arr[r][c] = sc.nextInt();
-                }
-            }
-            // 행 검사
-            for (int r = 0; r < 9; r++) {
-                int[] check = new int[9];
-                for (int c = 0; c < 9; c++) {
-                    check[arr[r][c]-1] ++;
-                }
-                for (int c = 0; c < 9; c++) {
-                    if (check[c] == 0) {
-                        flag = true;
-                    }
-                }
-            }
-
-            // 열 검사
-            for (int c = 0; c < 9; c++) {
-                int[] check = new int[9];
-                for (int r = 0; r < 9; r++) {
-                    check[arr[r][c]-1]++;
-                }
-                for (int r = 0; r < 9; r++) {
-                    if (check[r] == 0) {
-                        flag = true;
-                    }
-                }
-            }
-
-            // 3x3 검사
-            for (int i = 1; i < 9; i +=3) {
-                for (int j = 1; j < 9; j += 3) {
-                    int[] check = new int[9];
-                    check[arr[i][j]-1]++;
-                    for (int d = 0; d < 8; d++) {
-                        int ni = i + dr[d];
-                        int nj = j + dc[d];
-                        check[arr[ni][nj]-1]++;
-                        // check배열에서 -1을 하는 이유
-                        // 스도쿠 안에는 1~9까지 있으나 check배열은 배열번호가 0~8로 이뤄져있어 범위를 벗어난다.
-                    }
-                    for (int r = 0; r < 9; r++) {
-                        if (check[r] == 0) {
-                            flag = true;
-                        }
-                    }
-                }
-                
-            }
-
-            if (flag == false) {
-                System.out.printf("#%d 1\n", k);
-            } else {
-                System.out.printf("#%d 0\n", k);
-            }
-        }
-
-
-    }
+	
+	static int[] dr = {-1, 1, 0, 0, -1, -1, 1, 1};
+	static int[] dc = {0, 0, -1, 1, -1, 1, -1, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st; StringBuilder sb;
+		
+		int testCase = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <= testCase; t++) {
+			int[][] arr = new int[9][9];
+			boolean answer = true;
+			
+			// 전체 스도쿠 배열 입력
+			for(int r = 0; r < 9; r++){
+				st = new StringTokenizer(br.readLine());
+				for(int c = 0; c < 9; c++) {
+					arr[r][c] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			PriorityQueue<Integer> pq = new PriorityQueue<>();
+			for(int r = 0; r < 9; r++) {
+				for(int c = 0; c < 9; c++) { // 가로탐색, 한줄씩 우선큐에 입력
+					pq.offer(arr[r][c]);
+				}
+				for(int c = 1; c <= 9; c++) { // 하나씩 출력하면서 1씩 증가하는 값이 큐에서 출력되는 값과 동일한지 확인
+					if(pq.poll() != c) {
+						answer = false; // 다르다면 즉시 false
+					}
+				}
+				for(int c = 0; c < 9; c++) { // 세로탐색
+					pq.offer(arr[c][r]);
+				}
+				for(int c = 1; c <= 9; c++) {
+					if(pq.poll() != c) {
+						answer = false;
+					}
+				}
+			}
+		
+			// 3 X 3 탐색
+			for(int r = 1; r < 9; r+=3) {
+				for(int c = 1; c < 9; c+=3) {
+					pq.add(arr[r][c]);
+					for(int d = 0; d < 8; d++) {
+						int nr = r + dr[d];
+						int nc = c + dc[d];
+						pq.add(arr[nr][nc]);
+					}
+					for(int i = 1; i <= 9; i++) {
+						if(pq.poll() != i) {
+							answer = false;
+						}
+					}
+				}
+			}
+			
+			if(answer) {
+				System.out.printf("#%d %d\n", t, 1);
+			} else {
+				System.out.printf("#%d %d\n", t, 0);
+			}
+		}		
+	}
 }
