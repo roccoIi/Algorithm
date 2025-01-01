74e0dce09ea8b686b63385fcc49db357763e78a1
roccoIi <155591951+roccoIi@users.noreply.github.com>
2025-01-01 16:54:35 +0900

    [Gold V] Title: 춘배가 선물하는 특별한 하트, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Gold/30408. 춘배가 선물하는 특별한 하트/춘배가 선물하는 특별한 하트.java
+++ 백준/Gold/30408. 춘배가 선물하는 특별한 하트/춘배가 선물하는 특별한 하트.java
@@ -9,15 +9,13 @@ public class Main {
 		
 		long N = Long.parseLong(st.nextToken());
 		long M = Long.parseLong(st.nextToken());
-		TreeSet<Long> set = new TreeSet<>((s1, s2) -> Long.compare(s2, s1));
+		HashSet<Long> set = new HashSet<>();
+		Queue<Long> q = new LinkedList<>();
 		set.add(N);
+		q.add(N);
 		
-		while(!set.isEmpty()) {
-			// 자료구조 Set을 iterator로 변환시킨다.
-			Iterator<Long> iterator = set.iterator();
-			
-			long num = iterator.next();
-			iterator.remove(); // 이제 필요없으니 삭제
+		while(!q.isEmpty()) {
+			long num = q.poll();
 			
 			// 목표로 하는 숫자와 일치한다면 YES반환 후 while문 종료;
 			if(num == M) {
@@ -25,11 +23,21 @@ public class Main {
 				break;
 			}
 			
-			// 아직 목표 숫자보다 크다면 나눠서 다시 set에 넣는다.
+			// 아직 목표 숫자보다 크다면 나눠서 다시 queue와 set에 넣는다.
 			// 목표 숫자보다 작아졌다면 어차피 나눠도 목표숫자 도달 못하니 버린다.
 			if(num > M) {
-				set.add(num / 2);
-				if(num % 2 == 1) set.add(num / 2 + 1);
+				long divideNum = num / 2;
+				long divideNumPlusOne = num / 2 + 1;
+				
+				if(!set.contains(divideNum)) {
+					set.add(divideNum);
+					q.add(divideNum);
+				}
+				
+				if(num % 2 == 1 && !set.contains(divideNumPlusOne)) {
+					set.add(divideNumPlusOne);
+					q.add(divideNumPlusOne);
+				}
 			}
 		}
 		
