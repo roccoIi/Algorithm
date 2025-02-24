bd4daa32a894b93ad5e5e006d9d37d9571ff58d7
roccoIi <155591951+roccoIi@users.noreply.github.com>
2025-02-24 10:32:59 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 808 ms, Memory: 101780 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int R, C, map[][], dp[][];
+	static int R, C, map[][];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,33 +10,22 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		map = new int[R][C];
-		dp = new int[R][C];
-		int maxNum = 0;
-		for(int r = 0; r < R; r++) {
+		map = new int[R+1][C+1];
+
+		for(int r = 1; r <= R; r++) {
 			String[] str = br.readLine().split("");
-			for(int c = 0; c < C; c++) {
-				map[r][c] = Integer.parseInt(str[c]);
-				if(map[r][c] == 1) {
-					if(r == 0) dp[r][c] = 1;
-					if(c == 0) dp[r][c] = 1;
-					maxNum = dp[r][c] > maxNum ? dp[r][c] : maxNum;
-				}
+			for(int c = 1; c <= C; c++) {
+				map[r][c] = Integer.parseInt(str[c-1]);
 			}
 		}
 		
-		
-		for(int r = 1; r < R; r++) {
-			for(int c = 1; c < C; c++) {
+		int maxNum = 0;
+		for(int r = 1; r <= R; r++) {
+			for(int c = 1; c <= C; c++) {
 				if(map[r][c] == 0) continue;
 				
-				if(dp[r-1][c-1] > 0 && dp[r-1][c] > 0 && dp[r][c-1] > 0) {
-					dp[r][c] = Math.min(Math.min(dp[r-1][c-1], dp[r-1][c]), dp[r][c-1]) + 1;
-				} else {
-					dp[r][c] = 1;
-				}
-				
-				maxNum = dp[r][c] > maxNum ? dp[r][c] : maxNum;
+				map[r][c] = Math.min(Math.min(map[r-1][c-1], map[r-1][c]), map[r][c-1]) + 1;
+				maxNum = map[r][c] > maxNum ? map[r][c] : maxNum;
 			}
 		}
 		
