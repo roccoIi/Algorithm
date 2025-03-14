d95d94471a88f1d3639a9daada0bce5a55af1db9
roccoIi <155591951+roccoIi@users.noreply.github.com>
2025-03-14 15:09:38 +0900

    [Gold IV] Title: 서울에서 경산까지, Time: 352 ms, Memory: 61080 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.java
+++ 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.java
@@ -10,46 +10,38 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         
-        dp = new int[N][K+1];
-        Queue<Integer> q = new ArrayDeque<>();
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 2; i++) {
-        	int time = Integer.parseInt(st.nextToken());
-        	int money = Integer.parseInt(st.nextToken());
-        	q.add(time);
-        	dp[0][time] = money;
-        }
+        dp = new int[N+1][K+1];
         
-        int minTime = 0;
-        for(int r = 1; r < N; r++) {
+        for(int r = 1; r <= N; r++) {
         	st = new StringTokenizer(br.readLine());
         	int walkTime = Integer.parseInt(st.nextToken());
         	int walkMoney = Integer.parseInt(st.nextToken());
         	int bikeTime = Integer.parseInt(st.nextToken());
         	int bikeMoney = Integer.parseInt(st.nextToken());     	
-        	minTime += Math.min(walkTime, bikeTime);
         	
-        	int qSize = q.size();
-        	for(int c = 0; c < qSize; c++) {       	
-        		int lastTime = q.poll();
-        		
-        		if(lastTime + walkTime <= K) {
-        			int newTime = lastTime + walkTime;
-        			dp[r][newTime] = Math.max(dp[r][newTime], dp[r-1][lastTime] + walkMoney);
-        			q.add(newTime);
-        		}
-        		
-        		if(lastTime + bikeTime <= K) {
-        			int newTime = lastTime + bikeTime;
-        			dp[r][newTime] = Math.max(dp[r][newTime], dp[r-1][lastTime] + bikeMoney);
-        			q.add(newTime);
-        		}
-        	}	
+        	if(r == 1) {
+				dp[r][walkTime] = walkMoney;
+				dp[r][bikeTime] = Math.max(dp[r][bikeTime], bikeMoney);
+    		} else {
+	        	for (int t = 0; t <= K; t++) {
+	        		if(dp[r-1][t]==0) continue;
+	        		
+	                // 도보 선택
+	                if (t + walkTime <= K) {
+	                    dp[r][t + walkTime] = Math.max(dp[r][t + walkTime], dp[r - 1][t] + walkMoney);
+	                }
+	                
+	                // 자전거 선택
+	                if (t + bikeTime <= K) {
+	                    dp[r][t + bikeTime] = Math.max(dp[r][t + bikeTime], dp[r - 1][t] + bikeMoney);	
+	                }
+	        	} 
+            }
         }
         
         int answer = 0;
-        for(int i = minTime; i <= K; i++) {
-        	answer = Math.max(answer, dp[N-1][i]);
+        for (int t = 0; t <= K; t++) {
+        	answer = Math.max(answer, dp[N][t]);
         }
         System.out.println(answer);
     }
